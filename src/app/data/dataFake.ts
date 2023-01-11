1e1cba1d80e3e78d57811f945c28ada4945a9918
André Conde <andre.conde@aluno.ifsp.edu.br>
2023-01-11 09:13:06 -0300

    Mudança nos dados

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -8,7 +8,7 @@ export const dataFake = [
   {
     "id":"2",
     "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
+    "description": "Texto Top",
     "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
   }
 ]
